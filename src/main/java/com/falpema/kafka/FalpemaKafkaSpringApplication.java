4280863cdb1df301275d518265a73914e94df6fa
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-31 16:50:23 -0500

    create liste method of message of kafka

--- src/main/java/com/falpema/kafka/FalpemaKafkaSpringApplication.java
+++ src/main/java/com/falpema/kafka/FalpemaKafkaSpringApplication.java
@@ -1,11 +1,21 @@
 package com.falpema.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class FalpemaKafkaSpringApplication {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(FalpemaKafkaSpringApplication.class);
+	
+	@KafkaListener(topics = "falpema-topic", groupId= "falpema-group")
+	public void listen(String message) {
+		log.info("Message received",message);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FalpemaKafkaSpringApplication.class, args);
 	}
