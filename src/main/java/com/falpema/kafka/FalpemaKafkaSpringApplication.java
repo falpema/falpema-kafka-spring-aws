7757b3c1ca16554189a86ef244a71ae04f265427
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-31 18:42:18 -0500

    creation the logic of producer

--- src/main/java/com/falpema/kafka/FalpemaKafkaSpringApplication.java
+++ src/main/java/com/falpema/kafka/FalpemaKafkaSpringApplication.java
@@ -2,14 +2,20 @@ package com.falpema.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class FalpemaKafkaSpringApplication {
+public class FalpemaKafkaSpringApplication implements CommandLineRunner {
 	
 	private static final Logger log = LoggerFactory.getLogger(FalpemaKafkaSpringApplication.class);
+	
+	@Autowired
+	private KafkaTemplate<String,String> kafkaTemplate;
 	
 	@KafkaListener(topics = "devs4j-topic", groupId= "devs4j-group")
 	public void listen(String message) {
@@ -18,6 +24,12 @@ public class FalpemaKafkaSpringApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FalpemaKafkaSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		 kafkaTemplate.send("devs4j-topic","Sample message");
+		
 	}
 
 }
--- src/main/java/com/falpema/kafka/config/KafkaConfiguration.java
+++ src/main/java/com/falpema/kafka/config/KafkaConfiguration.java
@@ -4,16 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 public class KafkaConfiguration {
+	
+	public Map<String,Object> producerProperties(){
+		Map<String, Object> props=new HashMap<>(); 
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.RETRIES_CONFIG, 0);
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384); 
+		props.put(ProducerConfig.LINGER_MS_CONFIG, 1); 
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432); 
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class ); 
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return props;
+	}
+	
+
 	public Map<String, Object> consumerProperties() {
 
 		Map<String, Object> props = new HashMap<>();
@@ -22,6 +41,13 @@ public class KafkaConfiguration {
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		return props;
+	}
+	
+	@Bean
+	public KafkaTemplate<String,String> KafkaTemplate(){
+		DefaultKafkaProducerFactory<String,String> producerFactory = new DefaultKafkaProducerFactory<>(producerProperties());
+		KafkaTemplate<String, String> template = new KafkaTemplate<> (producerFactory);
+		return template;
 	}
 	
 	@Bean
