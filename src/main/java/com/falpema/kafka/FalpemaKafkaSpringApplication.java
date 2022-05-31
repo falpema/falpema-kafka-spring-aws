70ee9740e27baea3046c96f900a08d114cf529c2
Admin <admin@MacBook-Air-de-Fabian.local>
2022-05-31 16:40:53 -0500

    first commit structure of project

---
+++ src/main/java/com/falpema/kafka/FalpemaKafkaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.falpema.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalpemaKafkaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalpemaKafkaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falpema/kafka/FalpemaKafkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falpema.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalpemaKafkaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
